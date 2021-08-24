61fb7758a1a8d21f88557a84c8c82aa79aa980e4
usrwatermelon <kyntltntn@protonmail.com>
2021-08-24 16:30:03 +0000

    calculator can now store a second value

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-    const add = function(n1, n2) {
-    let simpleSum = n1 + n2;
+    const add = function(firstNum, secondNum) {
+    let simpleSum = firstNum + secondNum;
     return simpleSum;
     };
     
     const subtract = function(n1, n2) {
-      let difference = n1 -n2;
+      let difference = n1 - n2;
       return difference;
     };
     
@@ -24,20 +24,33 @@
         })
         return finalProduct
     };
+
+    const simpleMultiply = function(n1, n2){
+      let product = n1*n2;
+      return product;
+    }
+
     
-    const power = function(num1, num2) {
-        return Math.pow(num1, num2);
-    };
-    
-    const factorial = function(n) {
-        return n > 1 ? n * factorial(n - 1) : 1;
-    };
-    
-    function operate(operator, n1, n2){
-        if (operator === 'add'){
-        let result = add(n1, n2);
-        return result;
-        }
+    function operate(operator, firstNum, secondNum){
+        switch (operator){
+
+        case 'add':
+        result = add(firstNum, secondNum);
+        break;
+
+        case subtract:
+        result = subtract(firstNum, secondNum);
+        break;
+
+        case divide:
+        result = divide(firstNum, secondNum);
+        break;
+
+        case multiply:
+        result = simpleMultiply(firstNum, secondNum)
+        break;
+      }
+      return result;
     }
 
     function removeTransition(e){
@@ -70,21 +83,46 @@
   clearBtn.addEventListener('click', () => calcDisplay.textContent = 0);
   
   let firstNum = '';
+  let secondNum = '';
+  let operation = '';
+  let n1;
+  let n2;
   
   // WORK ON THIS
-  // const addBtn = document.querySelector('#add');
-  // addBtn.addEventListener('click', () => {
-  //   operator =
+  const addBtn = document.querySelector('#add');
+  addBtn.addEventListener('click', () => {
+    operator = 'add';
+    operation = 'active';
+    calcDisplay.textContent = '0';
+  })
 
-  // })
+  const equalsBtn = document.querySelector('#operate');
+  equalsBtn.addEventListener('click', () => {
+  operate(operator, firstNum, secondNum);
+  })
 
 // WORK ON THIS
 let addFirstNum = numberBtnsArr.forEach( btn => {
   btn.addEventListener('transitionend', removeTransition)
   btn.addEventListener('click', () => {
-    if (calcDisplay.textContent === '0'){
-        calcDisplay.textContent = btn.textContent;
+    if (operation === 'active' && calcDisplay.textContent === '0'){
+      calcDisplay.textContent = btn.textContent;
+      secondNum += btn.textContent;
+      btn.classList.add('activebtn')
     }
+
+    else if (operation === 'active'){
+    calcDisplay.append(btn.textContent);
+    secondNum += btn.textContent;
+    btn.classList.add('activebtn')
+    }
+
+    else if (calcDisplay.textContent === '0'){
+        calcDisplay.textContent = btn.textContent;
+        firstNum =+ btn.textContent;
+        btn.classList.add('activebtn')
+    }
+
     else {
     calcDisplay.append(btn.textContent);
     firstNum += btn.textContent;
