74f7ef82b6ace2e9353ec4a756e7a87eccd7d842
usrwatermelon <kyntltntn@protonmail.com>
2021-09-03 13:14:46 +0000

    cleaned js file comments, and added a readme markdown

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+  <h1 align="center">Calculator App</h1>
+  <p align="center">
+    <strong>This was the final project for The Odin Project fundamentals,before choosing a path between JavaScript or Ruby-on-Rails. It is meant to demonstrate the skills and knowledge gained for HTML, CSS, and JavaScript fundamentals. Also, I ended up choosing the JavaScript path.</strong>
+     <br /><br />
+    <a href="https://keaneatl.github.io/calculator/" target="_blank"><strong>Live Preview »</strong></a>
+    <br /><br />
+  </p>
+</p>
+
+<a href="https://keaneatl.github.io/calculator/" target="_blank"><img src="/images/livepreview.png" alt="Calculator App Live Preview"></a>
+
+<h2 style="display: inline-block">Table of Contents</h2>
+<ol>
+  <li>
+    <a href="#about-the-project">About The Project</a>
+    <ul>
+      <li><a href="#made-with">Made With</a></li>
+      <li><a href="#things-i-learned">Things I Learned</a></li>
+      <li><a href="#areas-of-improvement">Areas of Improvement</a></li>
+    </ul>
+  </li>
+
+  <li><a href="#disclaimer">Disclaimer</a></li>
+  <li><a href="#contact">Contact</a></li>
+</ol>
+
+## About The Project
+### Made With
+1. HTML
+2. CSS
+3. JavaScript
+
+### Things I Learned
+1. Utilizing functions that directly interact with HTML Elements.
+2. Making transitions with Event Listeners (for the buttons).
+
+### Areas of improvement
+1. Involving arrays more, as well as array methods.
+2. Making use of callback functions to use in Event Listeners for cleaner & more efficient code.
+
+## Disclaimer
+The resources I used, specifically the logo photoshop template, custom fonts installed, backgrounds, pictures, and the svg icons are only utilized for personal use, and not otherwise. 
+
+## Contact
+Keane Andre - @keaneatl (Instagram, Facebook, Twitter) - <a href="mailto:kyntltntn@protonmail.com">kyntltntn@protonmail.com</a>
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,85 @@
-    const add = function(firstNum, secondNum) {
-    let simpleSum = firstNum + secondNum;
-    return simpleSum;
-    };
+// FUNCTIONS FOR OPERATIONS 
+const add = function(firstNum, secondNum) {
+  let simpleSum = firstNum + secondNum;
+  return simpleSum;
+  };
     
-    const subtract = function(n1, n2) {
-      let difference = n1 - n2;
-      return difference;
-    };
+const subtract = function(n1, n2) {
+  let difference = n1 - n2;
+  return difference;
+  };
     
-    const sum = function(numbers) {
-        let finalSum = 0;
-      numbers.forEach(item => {
-        finalSum += item;
-        })
-        return finalSum
-    }
+const sum = function(numbers) {
+  let finalSum = 0;
+  numbers.forEach(item => {
+    finalSum += item;
+    })
+  return finalSum
+  }
     
-    const divide = function(firstNum, secondNum){
-      let quotient = firstNum / secondNum;
-      return quotient;
-    }
+const divide = function(firstNum, secondNum){
+  let quotient = firstNum / secondNum;
+  return quotient;
+  }
     
-    const multiply = function(nums) {
-        let finalProduct = 1;
-      nums.forEach(item => {
-        finalProduct *= item;
-        })
-        return finalProduct
-    };
+const multiply = function(nums) {
+  let finalProduct = 1;
+  nums.forEach(item => {
+    finalProduct *= item;
+      })
+  return finalProduct
+  };
 
-    const simpleMultiply = function(n1, n2){
-      let product = n1*n2;
-      return product;
-    }
+const simpleMultiply = function(n1, n2){
+  let product = n1*n2;
+  return product;
+  }
+ 
+function operate(operator, firstNum, secondNum){
+  switch (operator){
+    case 'add':
+    result = add(firstNum, secondNum);
+    break;
 
-    
-    function operate(operator, firstNum, secondNum){
-        switch (operator){
+    case 'subtract':
+    result = subtract(firstNum, secondNum);
+    break;
 
-        case 'add':
-        result = add(firstNum, secondNum);
-        break;
+    case 'divide':
+    result = divide(firstNum, secondNum);
+    break;
 
-        case 'subtract':
-        result = subtract(firstNum, secondNum);
-        break;
-
-        case 'divide':
-        result = divide(firstNum, secondNum);
-        break;
-
-        case 'multiply':
-        result = simpleMultiply(firstNum, secondNum)
-        break;
+    case 'multiply':
+    result = simpleMultiply(firstNum, secondNum)
+    break;
       }
-      return result;
+  return result;
     }
 
-    function removeTransition(e){
-      if (e.propertyName !== 'transform')return
-      this.classList.remove('activebtn');
-    }
+function removeTransition(e){
+  if (e.propertyName !== 'transform')return
+    this.classList.remove('activebtn');
+}
 
-  const calcDisplay = document.querySelector('.calc-display');
+// ELEMENTS SELECTORS
+const calcDisplay = document.querySelector('.calc-display');
   
-  const numberBtns = document.querySelectorAll('.calc-numbtn');
+const numberBtns = document.querySelectorAll('.calc-numbtn');
 
-  const decimalBtn = document.querySelector('#decimal');
+const decimalBtn = document.querySelector('#decimal');
 
-  const clearBtn = document.querySelector('.calc-clearbtn');
+const clearBtn = document.querySelector('.calc-clearbtn');
 
-  const operatorBtns = document.querySelectorAll('.calc-operator')
+const operatorBtns = document.querySelectorAll('.calc-operator')
 
-  const numberBtnsArr = Array.from(numberBtns);
+const numberBtnsArr = Array.from(numberBtns);
 
-  const operatorBtnsArr = Array.from(operatorBtns);
+const operatorBtnsArr = Array.from(operatorBtns);
   
-  const operatorBtnTransition = operatorBtnsArr.forEach( btn => {
-    btn.addEventListener('transitionend', removeTransition)
-    btn.addEventListener('click', () => btn.classList.add('activebtn'))
-  })
+const operatorBtnTransition = operatorBtnsArr.forEach( btn => {
+  btn.addEventListener('transitionend', removeTransition)
+  btn.addEventListener('click', () => btn.classList.add('activebtn'))
+})
 
   decimalBtn.addEventListener('click', () => {
     let displayArr = Array.from(calcDisplay.textContent);
@@ -99,7 +99,7 @@
     operator = '';
   })
   
-  
+  // VALUES AND OPERATOR VARIABLES
   let firstNum = '';
   let secondNum = '';
   let operation = '';
@@ -207,7 +207,7 @@
     }
   })
 
-// WORK ON THIS
+// CALCULATOR BUTTONS' EVENT LISTENERS
 let displayNum = numberBtnsArr.forEach( btn => {
   btn.addEventListener('transitionend', removeTransition)
   btn.addEventListener('click', () => {
@@ -243,6 +243,4 @@ let displayNum = numberBtnsArr.forEach( btn => {
     btn.classList.add('activebtn')
     }
   })
-
-  
 })
